e0b110cb784911e745d5154ea9b570e86beaeb93
Akshat Khandelwal <ak127a@gmail.com>
2025-03-16 20:54:43 +0530

    Add vim-fugitive, set some keybinds for gitsigns

---
+++ lua/plugins/git_stuff.lua
@@ -0,0 +1,15 @@
+return { -- Adds git related signs to the gutter, as well as utilities for managing changes
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+
+			-- keybinds
+			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>")
+			vim.keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>")
+		end,
+	},
+	{
+		"tpope/vim-fugitive",
+	},
+}
