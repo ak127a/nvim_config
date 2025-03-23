261abffbc7b77c5a6cd9ae8af6c90a07babcea6a
akskhand <akshat.khandelwal@oracle.com>
2025-03-23 22:59:48 +0530

    Added some more keybinds for diffview

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,19 +1,34 @@
+local has_git = vim.fn.executable("git") == 1
 return {
-	"NeogitOrg/neogit",
-	dependencies = {
-		"nvim-lua/plenary.nvim", -- required
-		"sindrets/diffview.nvim", -- optional - Diff integration
-
-		-- Only one of these is needed.
-		"nvim-telescope/telescope.nvim", -- optional
+	{
+		"sindrets/diffview.nvim",
+		cond = has_git,
+		opts = {
+			enhanced_diff_hl = true, -- See ':h diffview-config-enhanced_diff_hl'
+		},
+		config = function()
+			local actions = require("diffview.actions")
+			vim.keymap.set("n", "<leader>gd", "<cmd>DiffviewOpen -uno<CR>")
+			vim.keymap.set("n", "<leader>gD", "<cmd>DiffviewOpen master<CR>")
+			vim.keymap.set("n", "<leader>gh", "<cmd>DiffviewFileHistory %<CR>")
+			vim.keymap.set("n", "q", actions.close)
+			vim.keymap.set("n", "<C-r>", actions.refresh_files)
+		end,
 	},
-	config = function()
-		local neogit = require("neogit")
-		neogit.setup({})
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
 
-		-- remaps
-		vim.keymap.set("n", "<leader>gg", "<cmd>Neogit<CR>")
-		vim.keymap.set("n", "<leader>gd", "<cmd>DiffviewOpen -uno<CR>")
-		vim.keymap.set("n", "<leader>gD", "<cmd>DiffviewOpen master<CR>")
-	end,
+			-- Only one of these is needed.
+			"nvim-telescope/telescope.nvim", -- optional
+		},
+		config = function()
+			local neogit = require("neogit")
+			neogit.setup({})
+
+			vim.keymap.set("n", "<leader>gg", "<cmd>Neogit<CR>")
+		end,
+	},
 }
